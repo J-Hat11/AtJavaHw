f8d4b7507fd3020bd80775e8e2cfdf04adef497c
J-Hat11 <27jhatfield@derryfield.org>
2025-12-16 22:55:24 -0500

    Lesson13Graphics

---
+++ Lesson13(graphics)/PartC.java
@@ -0,0 +1,128 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.*;
+
+ public class PartC {
+    /*
+     * Change the following method to customize
+     * what is drawn in the JFrame.
+     */
+    public static void draw(Graphics g) {
+        // by default, this method draws a blue square.
+        g.setColor(Color.PINK);
+        g.fillRect(0, 0, 400, 400);
+        Rose(g);
+        
+    }//used internet for help on this problem...Specifically regadring the inside of the for loop and the variables with stars next to them
+    public static void Rose(Graphics g){
+        int Height=400;
+        int Width=400;
+        double scale = 150.0;
+        int steps = 1000;
+
+        int centerx = Width/2;//* 
+        int centery = Height/2;//* 
+
+        int PrevX = centerx;//* 
+        int PrevY = centery; //* 
+
+
+        for (int i =0; i<=steps; i++ ){
+            double Theta = (2*Math.PI/steps)*i;
+            double r = Math.cos(2*Theta);
+            double x = scale * r * Math.cos(Theta);
+            double y = scale * r * Math.sin(Theta);
+            int Plotx = centerx + (int)x;
+            int Ploty = centery - (int)y;
+
+            if (i>1){
+                g.setColor(Color.MAGENTA);
+                g.drawLine(PrevX, PrevY, Plotx, Ploty);
+            }
+            PrevX = Plotx;//* 
+            PrevY = Ploty;//* 
+        
+        }
+
+        
+
+        
+    }
+
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+
+        return val;
+    }
+
+    // Do not modify the code in the main method.
+    // Your code will go into the draw method above.
+    public static void main(String[] args) {
+        // this is the desired dimensions of the content area
+        // of our JFrame window.
+        final int CONTENT_WIDTH = 400;
+        final int CONTENT_HEIGHT = 400;
+      
+
+        // the values for border width and title bar height
+        // depend on the operating system.
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                // I don't know what the values are for Linux.
+                break;
+            case OTHER:
+                break;
+        }
+
+        int frame_width = CONTENT_WIDTH + 2 * border_width;
+        int frame_height = CONTENT_HEIGHT + bar_height + border_width;
+        JFrame frame = new JFrame();
+        frame.setSize(frame_width, frame_height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // set title
+        frame.setTitle("Graphics Template");
+
+        JComponent component = new JComponent() {
+            public void paintComponent(Graphics graph) {
+                draw(graph);
+            }
+        };
+
+        frame.add(component);
+        frame.setVisible(true);
+
+        System.out.println("Frame Size   : " + frame.getSize());
+        System.out.println("Frame Insets : " + frame.getInsets());
+        System.out.println("Content Size : " + frame.getContentPane().getSize());
+    }
+
+}
+
+
+
